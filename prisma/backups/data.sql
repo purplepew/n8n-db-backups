a11a8689cc0708c2874077f1dd286cd7dc9f0aa0
purplepew <purplepew@users.noreply.github.com>
2026-06-04 02:38:34 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict bfUOzFTZTSmDkp7ybN7M0MSlWEYRAkbxhjbC91RiVn8VI5rD8KHzzqVIyqMQtxF
+-- \restrict 5EkZ7UHNPKQ61IGVEDmbsJFGFygd8bKGn4EG63mrMgY6SO9xKPQrGMcDqCFIWj3
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -518,6 +518,6 @@ SELECT pg_catalog.setval('"public"."role_id_seq"', 6, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict bfUOzFTZTSmDkp7ybN7M0MSlWEYRAkbxhjbC91RiVn8VI5rD8KHzzqVIyqMQtxF
+-- \unrestrict 5EkZ7UHNPKQ61IGVEDmbsJFGFygd8bKGn4EG63mrMgY6SO9xKPQrGMcDqCFIWj3
 
 RESET ALL;
